c2f70a242a5cd62a61ffbef2df828f89a1e07f6e
lcoralie <coralie.lauryann@outlook.fr>
2018-02-01 11:37:03 +0100

    classe dimension

---
+++ Decoration/src/test/Dimension.java
@@ -0,0 +1,24 @@
+package test;
+
+public class Dimension {
+	private int lenght;
+	private int width;
+	
+	public Dimension (int lenght, int width){
+		this.lenght = lenght;
+		this.width = width;
+	}
+	public int getLenght() {
+		return lenght;
+	}
+	public void setLenght(int lenght) {
+		this.lenght = lenght;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	
+}
