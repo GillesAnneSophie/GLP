2930ef4d3337439a4cf1ed781f407f31361980d1
Chabani-Z <ahamadachabani@hotmail.fr>
2018-02-05 17:53:12 +0100

    .

---
+++ Decoration/src/categories/Floor.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package categories;
+
+import catalog.Category;
+
+/**
+ * @author Chabane
+ *
+ */
+public class Floor extends Category {
+
+	public String toString () {
+		
+		return "Floor" ;
+	}
+	
+}
