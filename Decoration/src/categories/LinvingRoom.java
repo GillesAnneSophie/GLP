43d23f696628960e73ec024b3136069ed90b3ae0
Chabani-Z <ahamadachabani@hotmail.fr>
2018-02-05 17:18:33 +0100

    .

---
+++ Decoration/src/categories/LinvingRoom.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package categories;
+
+import catalog.Category;
+
+/**
+ * @author Chabane
+ *
+ */
+public class LinvingRoom extends Category{
+	
+	public String toString ()
+	
+	{
+		
+		return "LivingRoom" ;
+	}
+	
+
+	
+	
+}
