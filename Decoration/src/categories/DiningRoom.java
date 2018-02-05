232cba1dded373f078ba714e39c27b721dd9d2a6
Chabani-Z <ahamadachabani@hotmail.fr>
2018-02-05 17:28:25 +0100

    .

---
+++ Decoration/src/categories/DiningRoom.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package categories;
+
+import catalog.Category;
+
+/**
+ * @author Chabane
+ *
+ */
+public class DiningRoom extends Category {
+	
+	public String toString() {
+		
+		return "DiningRoom";
+	}
+	
+
+}
