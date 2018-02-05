88b0e5395560a764f49cb6049348f497a60f37d2
Chabani-Z <ahamadachabani@hotmail.fr>
2018-02-05 17:40:23 +0100

    .

---
+++ Decoration/src/categories/Wall.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package categories;
+
+import catalog.Category;
+
+/**
+ * @author Chabane
+ *
+ */
+public class Wall extends Category  {
+	
+	public String toString() {
+		
+		return "Wall";
+	}
+
+}
