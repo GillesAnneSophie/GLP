7ae2738c6cb2d0669d8f8cd7935f5d024db16e54
Chabani-Z <ahamadachabani@hotmail.fr>
2018-02-05 17:05:48 +0100

    Premier test en retard , je crée les classes qui n'ont pas été faite.

---
+++ Decoration/src/categories/Kitchen.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package categories;
+
+import catalog.Category;
+
+/**
+ * @author Chabane
+ *
+ */
+public class Kitchen extends Category {
+	
+	public String toString()
+	
+	{
+		
+		return "Kitchen";
+			
+	}
+	
+	
+	
+
+}
+
